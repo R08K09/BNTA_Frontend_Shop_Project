0ae0216d66300a240f6eca69c023b4f6d62cafa8
Kelly Wong <kellykl_wong@hotmail.com>
2023-04-24 13:59:09 +0100

    adds project base

---
+++ shop__backend_project/src/test/java/com/example/shop__backend_project/ShopBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop__backend_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
