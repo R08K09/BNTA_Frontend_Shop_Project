5795d60c14a43007cf6a6a595e35656c9326d8c5
Your Name <you@example.com>
2023-04-25 15:08:40 +0100

    fixed conflict

--- shop__backend_project/src/main/java/com/example/shop__backend_project/controllers/CustomerController.java
+++ shop__backend_project/src/main/java/com/example/shop__backend_project/controllers/CustomerController.java
@@ -1,20 +1,14 @@
 package com.example.shop__backend_project.controllers;
 
 import com.example.shop__backend_project.models.Customer;
-<<<<<<< HEAD
-=======
 import com.example.shop__backend_project.repositories.CustomerRepository;
->>>>>>> develop
 import com.example.shop__backend_project.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-=======
 import org.springframework.web.bind.annotation.GetMapping;
->>>>>>> develop
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,15 +23,16 @@ public class CustomerController {
 
 //    Add new customer
     @PostMapping
-    public ResponseEntity<Customer> addNewCustomer (@RequestBody Customer customer){
+    public ResponseEntity<Customer> addNewCustomer (@RequestBody Customer customer) {
         Customer newCustomer = customerService.addNewCustomer(customer);
-        return  new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
-
-    @Autowired
-    CustomerRepository customerRepository;
-
-    @GetMapping
-    public ResponseEntity<List<Customer>> getAllCustomers(){
-        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
     }
+
+        @Autowired
+        CustomerRepository customerRepository;
+
+        @GetMapping
+        public ResponseEntity<List<Customer>> getAllCustomers () {
+            return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+        }
 }
