415e25d94d39fe849e5ad419a264375f851d7991
Kelly Wong <kellykl_wong@hotmail.com>
2023-04-25 15:13:31 +0100

    adds delete customer

--- shop__backend_project/src/main/java/com/example/shop__backend_project/controllers/CustomerController.java
+++ shop__backend_project/src/main/java/com/example/shop__backend_project/controllers/CustomerController.java
@@ -2,6 +2,10 @@ package com.example.shop__backend_project.controllers;
 
 import com.example.shop__backend_project.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,12 @@ public class CustomerController {
 
     @Autowired
     CustomerService customerService;
+
+//    DELETE customer by id
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Long> deleteCustomer(@PathVariable Long id){
+        customerService.deleteCustomer(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
 
 }
