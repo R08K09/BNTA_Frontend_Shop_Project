1536e4feb8b72504ab75b81a804b53c2d1b417c9
Theo Lloyd <theolloyd@Theos-MacBook-Air.local>
2023-04-25 10:42:55 +0100

    added find product by id

--- shop__backend_project/src/main/java/com/example/shop__backend_project/controllers/ProductController.java
+++ shop__backend_project/src/main/java/com/example/shop__backend_project/controllers/ProductController.java
@@ -2,10 +2,12 @@ package com.example.shop__backend_project.controllers;
 
 import com.example.shop__backend_project.models.Product;
 import com.example.shop__backend_project.services.ProductService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,9 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts(){
         return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> findProductById(@PathVariable Long id){
+        return new ResponseEntity(productService.findProductById(id), HttpStatus.OK);
     }
 }
