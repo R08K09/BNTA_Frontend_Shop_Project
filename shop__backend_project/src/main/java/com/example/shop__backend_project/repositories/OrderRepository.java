fcc3cf8732f41c248d8482c455ff1c843d20dd1f
Nasthasia Usoh <nasthasia@hotmail.co.uk>
2023-05-25 12:53:45 +0100

    fixing folders

--- shop__backend_project/src/main/java/com/example/shop__backend_project/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shop__backend_project.repositories;
-
-import com.example.shop__backend_project.models.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
