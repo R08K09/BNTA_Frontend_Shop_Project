1962de15190ff9e73d40dccb0ce62e2621251de2
William Dorling <williamdorling@hotmail.com>
2023-05-26 12:58:57 +0100

    added vehicle images

--- frontend_shop_project_client/src/components/Vehicle.js
+++ frontend_shop_project_client/src/components/Vehicle.js
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react";
+
+import {images} from "../images";
+
 const Vehicle = ({vehicle}) => {
+
+    const [inspecting, setInspecting] = useState(false);
+ 
+
+    const handleClick = () => {
+        setInspecting(!inspecting);
+    }
+
+    const foundImage = images.find(image => image.id === vehicle.id);
+
+    const imageComponent = <img src={foundImage.img} alt={vehicle.category} className="vehicle-image" />
 
     const displayPrice = (vehicle.price/100).toLocaleString();
     return (  
-        <div>
-            <h3>{vehicle.name}</h3>
-            <p>Price: £{displayPrice}.00</p>
-            <p>Colour: {vehicle.colour}</p>
-        </div>
+        <>
+            {inspecting ? 
+            <div className= "product-inspected" onClick={handleClick}>
+                {imageComponent}
+                <h3>{vehicle.name}</h3>
+                <p>Price: £{displayPrice}.00</p>
+                <p>Colour: {vehicle.colour}</p>
+                <button>Add to cart</button>
+            </div>
+            : 
+            <div className="product-not-inspected" onClick={handleClick}>
+                {imageComponent}
+                <h3>{vehicle.name}</h3>
+            </div>
+            }
+        </>
     );
 }
  
---
+++ frontend_shop_project_client/src/images.js
@@ -0,0 +1,40 @@
+let images = [
+    {
+        id: 7,
+        img: "https://e3.365dm.com/21/01/2048x1152/skynews-boeing-737-max_5251745.jpg?20210127112513"
+    },
+    {
+        id: 8,
+        img: "https://source.unsplash.com/n7pLpkhwXFk"
+    },
+    {
+        id: 9,
+        img: "https://source.unsplash.com/l4bde8nRFkY"
+    },
+    {
+        id: 1,
+        img: "https://source.unsplash.com/Zexjl0v3MRU"
+    },
+    {
+        id: 4,
+        img: "https://www.stratstone.com/-/media/stratstone/mercedes-benz/hero-banner/a-class.ashx"
+    },
+    {
+        id: 6,
+        img: "https://www.topgear.com/sites/default/files/images/cars-road-test/2019/05/543bbea95111e4b0bb6bfdca404e5767/large-32844-mercedes-benzg350d.jpg"
+    },
+    {
+        id: 5,
+        img: "https://f7432d8eadcf865aa9d9-9c672a3a4ecaaacdf2fee3b3e6fd2716.ssl.cf3.rackcdn.com/C2299/U678/IMG_70422-large.jpg"
+    },
+    {
+        id: 2,
+        img: "https://source.unsplash.com/FWJinfDsIn8"
+    },
+    {
+        id: 3,
+        img: "https://source.unsplash.com/XLghD0KHAck"
+    }
+]
+
+export {images};
